b79c60946e5a118d2a211531fa4dc44417e325b0
Kevin Zhuang <kevin7441@gmail.com>
2021-04-03 23:12:31 +1100

    test(app): more coverage

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 import pytest
 from prompt_toolkit.application import create_app_session
+from prompt_toolkit.application.application import Application
 from prompt_toolkit.input import create_pipe_input
 from prompt_toolkit.layout.containers import FloatContainer, VSplit
 from prompt_toolkit.output import DummyOutput
@@ -7,17 +8,23 @@ from prompt_toolkit.widgets.base import Frame
 from pytest_mock.plugin import MockerFixture
 
 from s3fm.api.config import Config
+from s3fm.api.history import History
 from s3fm.app import App
 from s3fm.exceptions import Bug
 from s3fm.id import LayoutMode
+from s3fm.ui.filepane import FilePane
 
 
 @pytest.fixture
 def app():
-    with create_app_session(input=create_pipe_input(), output=DummyOutput()):
-        config = Config()
-        app = App(config=config)
-        yield app
+    pipe_input = create_pipe_input()
+    try:
+        with create_app_session(input=pipe_input, output=DummyOutput()):
+            config = Config()
+            app = App(config=config)
+            yield app
+    finally:
+        pipe_input.close()
 
 
 class TestAppLayout:
@@ -57,3 +64,68 @@ class TestAppLayout:
         with pytest.raises(Bug):
             app._layout_mode = 5
             app.layout
+
+
+class TestRedraw:
+    class Stub:
+        def __init__(self, value):
+            self.value = value
+
+        def cancelled(self):
+            return self.value
+
+    def test_redraw_cancelled(self, app, mocker):
+        spy = mocker.spy(Application, "invalidate")
+        stub = self.Stub(True)
+        app.redraw(task=stub)
+        spy.assert_not_called()
+
+    def test_redraw(self, app, mocker):
+        spy = mocker.spy(Application, "invalidate")
+        stub = self.Stub(False)
+        app.redraw(task=stub)
+        spy.assert_called_once()
+
+    def test_redraw_empty(self, app, mocker):
+        spy = mocker.spy(Application, "invalidate")
+        app.redraw()
+        spy.assert_called_once()
+
+
+@pytest.mark.asyncio
+async def test_load_pane_date(app, mocker: MockerFixture):
+    class Stub:
+        async def load_data(self):
+            pass
+
+    spy = mocker.spy(App, "redraw")
+    await app._load_pane_data(Stub())
+    spy.assert_called_once()
+
+
+@pytest.mark.asyncio
+async def test_render_task(app, mocker: MockerFixture):
+    assert app._kb.activated == False
+    spy = mocker.spy(History, "read")
+    mocker.patch.object(App, "focus_pane")
+    mocker.patch.object(App, "switch_layout")
+    mocker.patch.object(App, "_load_pane_data")
+    app._no_history = True
+    await app._render_task()
+    assert app._kb.activated == True
+    spy.assert_not_called()
+
+
+@pytest.mark.asyncio
+async def test_after_render(app, mocker: MockerFixture):
+    stub = mocker.stub("app")
+    app._custom_effects = [stub]
+    task = mocker.patch.object(App, "_render_task")
+    mocker.patch.object(FilePane, "loading")
+    app._rendered = False
+    app._after_render(None)
+    task.assert_called_once()
+    task.reset_mock()
+    app._after_render(None)
+    task.assert_not_called()
+    stub.assert_called_with(app)
