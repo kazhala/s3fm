e8cca98ea36c7594073d2caf92dafd665220e17e
Kevin Zhuang <kevin7441@gmail.com>
2021-04-05 10:16:20 +1000

    refactor(api): use dataclass for file class

--- s3fm/api/file.py
+++ s3fm/api/file.py
@@ -1,10 +1,12 @@
 """Module contains the file object and helper methods."""
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Any, Dict, Optional, Union
 
 from s3fm.id import ID
 
 
+@dataclass
 class File:
     """Used to store basic file information.
 
@@ -23,48 +25,9 @@ class File:
         Path: Full path of the file.
     """
 
-    def __init__(
-        self,
-        name: str,
-        type: ID,
-        info: str,
-        hidden: bool,
-        index: int,
-        raw: Optional[Union[Path, Dict[str, Any]]],
-    ) -> None:
-        self._name = name
-        self._type = type
-        self._info = info
-        self._hidden = hidden
-        self._index = index
-        self._raw = raw
-
-    @property
-    def name(self) -> str:
-        """str: Name of the file."""
-        return self._name
-
-    @property
-    def type(self) -> ID:
-        """ID: FileType id."""
-        return self._type
-
-    @property
-    def info(self) -> str:
-        """str: File information."""
-        return self._info
-
-    @property
-    def hidden(self) -> bool:
-        """bool: Is hidden file."""
-        return self._hidden
-
-    @property
-    def index(self) -> int:
-        """int: Original file index."""
-        return self._index
-
-    @property
-    def raw(self) -> Optional[Union[Path, Dict[str, Any]]]:
-        """Optional[Union[Dict[str, Any], :obj:`pathlib.Path`]]: Full path of the file."""
-        return self._raw
+    name: str
+    type: ID
+    info: str
+    hidden: bool
+    index: int
+    raw: Optional[Union[Path, Dict[str, Any]]]
