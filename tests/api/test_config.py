6cedcff3f4da412e0adb6df4badfba2de854f248
Kevin Zhuang <kevin7441@gmail.com>
2021-05-16 16:41:27 +1000

    test(config): style

--- tests/api/test_config.py
+++ tests/api/test_config.py
@@ -1,6 +1,6 @@
 import pytest
 
-from s3fm.api.config import AppConfig, LineModeConfig
+from s3fm.api.config import AppConfig, BaseStyleConfig, LineModeConfig, StyleConfig
 
 
 def test_app_config():
@@ -24,3 +24,28 @@ def test_linemod_config():
         pass
 
     assert config._process == [what]
+
+
+def test_base_style_config():
+    class Config(BaseStyleConfig):
+        def __init__(self) -> None:
+            self.bar = "#ffffff"
+            self.header = "#000000"
+
+    config = Config()
+    assert dict(config) == {"bar": "#ffffff", "header": "#000000"}
+
+    config.clear()
+    assert dict(config) == {"bar": "", "header": ""}
+
+
+def test_style_config():
+    style_config = StyleConfig()
+
+    @style_config.register("hello")
+    class Hello(BaseStyleConfig):
+        def __init__(self) -> None:
+            self.file_color = "#ffffff"
+
+    assert hasattr(style_config, "hello") == True
+    assert dict(style_config)["hello.file_color"] == "#ffffff"
