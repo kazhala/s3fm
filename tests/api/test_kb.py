8007a174bc7b353852a42c13ee2df3999a33bd58
Kevin Zhuang <kevin7441@gmail.com>
2021-06-05 18:28:36 +1000

    test(kb): scroll

--- tests/api/test_kb.py
+++ tests/api/test_kb.py
@@ -3,10 +3,12 @@ from typing import NamedTuple
 import pytest
 from prompt_toolkit.key_binding.key_bindings import KeyBindings
 from prompt_toolkit.key_binding.key_processor import KeyPressEvent, KeyProcessor
+from pytest_mock.plugin import MockerFixture
 
 from s3fm.api.kb import KB
 from s3fm.app import App
-from s3fm.enums import KBMode
+from s3fm.enums import Direction, KBMode, LayoutMode
+from s3fm.ui.filepane import FilePane
 
 
 @pytest.fixture
@@ -46,3 +48,35 @@ def test_set_action_multiplier(kb):
     mocked_event = FakeEvent([FakeSequence("1")])
     kb._set_action_multiplier(mocked_event)
     assert kb._action_multiplier == 11
+
+
+def test_swap_pane(mocker: MockerFixture, kb):
+    mocked_swap = mocker.patch.object(App, "pane_swap")
+
+    kb._swap_pane(Direction.left)
+    mocked_swap.assert_called_with(Direction.left, layout=LayoutMode.vertical)
+
+    kb._swap_pane(Direction.down)
+    mocked_swap.assert_called_with(Direction.down, layout=LayoutMode.horizontal)
+
+
+def test_scroll_down(mocker: MockerFixture, kb):
+    mocked_scorll = mocker.patch.object(FilePane, "scroll_down")
+
+    kb._scroll_down()
+    mocked_scorll.assert_called_with(value=1, page=False, bottom=False)
+
+    kb._action_multiplier = 11
+    kb._scroll_down(page=True, bottom=True)
+    mocked_scorll.assert_called_with(value=11, page=True, bottom=True)
+
+
+def test_scroll_up(mocker: MockerFixture, kb):
+    mocked_scorll = mocker.patch.object(FilePane, "scroll_up")
+
+    kb._scroll_up()
+    mocked_scorll.assert_called_with(value=1, page=False, top=False)
+
+    kb._action_multiplier = 11
+    kb._scroll_up(page=True, top=True)
+    mocked_scorll.assert_called_with(value=11, page=True, top=True)
