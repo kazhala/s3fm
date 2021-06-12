3b0cee281a64999cf5797f879ae1c95433cf3e74
Kevin Zhuang <kevin7441@gmail.com>
2021-06-12 21:27:46 +1000

    test(kb): update

--- tests/api/test_kb.py
+++ tests/api/test_kb.py
@@ -20,6 +20,7 @@ def kb(app: App):
             KBMode.command: {"exit": [{"keys": "c-c"}]},
             KBMode.error: {"exit": [{"keys": "c-c"}]},
             KBMode.search: {},
+            KBMode.reverse_search: {},
         },
         custom_kb_maps={KBMode.normal: {"hello": [{"keys": "j"}]}, KBMode.command: {}},
         custom_kb_lookup={KBMode.normal: {"hello": lambda: True}, KBMode.command: {}},
