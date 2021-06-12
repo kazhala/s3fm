9930030a763ff9f1161081fc1451ddfd939bc8bb
Kevin Zhuang <kevin7441@gmail.com>
2021-06-12 12:11:48 +1000

    test(error): error pane test

---
+++ tests/ui/test_error.py
@@ -0,0 +1,43 @@
+import pytest
+
+from s3fm.app import App
+from s3fm.enums import ErrorType
+from s3fm.exceptions import Notification
+
+
+def test_get_title(app: App):
+    app.set_error(Notification(message="hello", error_type=ErrorType.error))
+    assert app._error_pane._get_title() == [("class:error.error", "ERROR")]
+    app.set_error(Notification(message="hello", error_type=ErrorType.warning))
+    assert app._error_pane._get_title() == [("class:error.warning", "WARNING")]
+    app.set_error(Notification(message="hello", error_type=ErrorType.info))
+    assert app._error_pane._get_title() == [("class:error.info", "INFO")]
+
+
+def test_get_text(app: App):
+    app.set_error(Notification(message="hello", error_type=ErrorType.error))
+    assert app._error_pane._get_text() == [
+        ("", "\n"),
+        ("class:error.error", "hello"),
+        ("", "\n"),
+        ("", "\n"),
+        ("class:error.instruction", "Press any key to continue ..."),
+    ]
+
+    app.set_error(Notification(message="hello", error_type=ErrorType.warning))
+    assert app._error_pane._get_text() == [
+        ("", "\n"),
+        ("class:error.warning", "hello"),
+        ("", "\n"),
+        ("", "\n"),
+        ("class:error.instruction", "Press any key to continue ..."),
+    ]
+
+    app.set_error(Notification(message="hello", error_type=ErrorType.info))
+    assert app._error_pane._get_text() == [
+        ("", "\n"),
+        ("class:error.info", "hello"),
+        ("", "\n"),
+        ("", "\n"),
+        ("class:error.instruction", "Press any key to continue ..."),
+    ]
